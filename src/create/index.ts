39d21f3f8381ffcb47a8ab62c00ab0a0dd22a2e9
QC-L <github@liqichang.com>
2019-10-29 16:54:05 +0800

    feat: add repo version check

--- src/create/index.ts
+++ src/create/index.ts
@@ -1,8 +1,11 @@
 import ora from 'ora';
 import clone from './clone';
+import { checkCurrentRepoVersion } from './check-version'
 import render, { ArgvType, pathAndRepoUrlGenerator } from './render';
 
-export default (argv: ArgvType, macros: MacrosType) => {
+export { ArgvType }
+export default async (argv: ArgvType, macros: MacrosType) => {
+  await checkCurrentRepoVersion(macros.scriptName)
   const spinner = ora(macros.download).start();
   const generatorValues = pathAndRepoUrlGenerator(argv, macros);
   const {
