2669aa825ada0efbf54c61ad3610ee4fe8f84938
QiChang Li <github@liqichang.com>
2019-11-03 11:54:00 +0800

    feat: add template and repo version check (#5)
    * feat: add repo version check
    
    * feat: add template version check

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -2,6 +2,12 @@ declare module 'degit';
 declare module 'metalsmith';
 declare module 'metalsmith-markdown';
 declare module 'consolidate';
+declare module 'semver';
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
 
 interface ObjectValueType {
   [key: string]: string | boolean | { [key: string]: any }
--- src/create/check-version.ts
+++ src/create/check-version.ts
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import semver from 'semver';
+import chalk from 'chalk';
+import path from 'path';
+import fs from 'fs';
+
+import packageConfig from '../../package.json'
 
 const checkRepoVersion = async (repoName: string) => {
   const { tag_name } = await getResult(`https://api.github.com/repos/${repoName}/releases/latest`)
@@ -8,9 +14,59 @@ const checkRepoVersion = async (repoName: string) => {
 const getResult = (url: string) => {
   return axios.get(url).then(data => {
     return data.data
-  }).catch(e => {
-    // console.error(e)
   })
 }
 
-export { checkRepoVersion }
+const checkCurrentTemplateVersion = async (macros: MacrosType, isTypeScript: boolean, tmpPath: string) => {
+  let repoName = isTypeScript ? macros.templateRepo : macros.templateTSRepo
+  const pkgPath = path.join(tmpPath, 'package.json')
+  
+  if (fs.existsSync(pkgPath)) {
+    const tmpPackageConfig = require(pkgPath)
+    const latestVersion = await checkRepoVersion(repoName)
+    return semver.lt(tmpPackageConfig.version, latestVersion)
+  } else {
+    return true
+  }
+}
+
+const checkCurrentRepoVersion = async (repoName: string) => {
+  try {
+    const res = await getResult(`https://registry.npmjs.org/${repoName}`)
+    const latestVersion = res['dist-tags'].latest
+    const localVersion = packageConfig.version
+    if (semver.lt(localVersion, latestVersion)) {
+      const placeholder = '*                                                     *'
+      const origin = '*                                                               *'
+      const latest = 'latest:    ' + chalk.green(latestVersion)
+      const installed = 'installed: ' + chalk.red(localVersion)
+      const longLength = latest.length > installed.length ? latest.length : installed.length
+
+      console.log('*******************************************************')
+      console.log(placeholder)
+      console.log('*', chalk.yellow(` A newer version of ${repoName} is available. `), '*')
+      console.log(placeholder)
+      console.log(generatorPlace(origin, latest, longLength))
+      console.log(generatorPlace(origin, installed, longLength))
+      console.log(placeholder)
+      console.log('*******************************************************')
+    }
+  } catch (err) {
+    // console.log(err)
+  }
+
+}
+
+const generatorPlace = (origin: string, replace: string, longLength: number): string => {
+  const index = Math.round(origin.length / 2) - Math.round(longLength / 2) - 1
+  const length = replace.length
+  let originArray = origin.split('')
+  const replaceArray = replace.split('')
+  for (let i = 0; i < length; i++) {
+    originArray[i + index] = replaceArray[i]
+  }
+
+  return originArray.join('')
+}
+
+export { checkRepoVersion, checkCurrentRepoVersion, checkCurrentTemplateVersion }
