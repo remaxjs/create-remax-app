a93901934064c9d5cd537f528e2d669bf56a520e
QiChang Li <github@liqichang.com>
2019-11-20 15:52:20 +0800

    refactor: check version (#8)

--- src/create/check-version.ts
+++ src/create/check-version.ts
@@ -4,17 +4,21 @@ import chalk from 'chalk';
 import path from 'path';
 import fs from 'fs';
 
-import packageConfig from '../../package.json'
-
-const checkRepoVersion = async (repoName: string) => {
-  const { tag_name } = await getResult(`https://api.github.com/repos/${repoName}/releases/latest`)
-  return tag_name
+export interface VersionValueType extends ObjectValueType {
+  localVersion: string;
+  latestVersion: string;
+  repoName: string;
 }
 
 const getResult = (url: string) => {
   return axios.get(url).then(data => {
     return data.data
   })
+}
+
+const checkRepoVersion = async (repoName: string) => {
+  const { tag_name } = await getResult(`https://api.github.com/repos/${repoName}/releases/latest`)
+  return tag_name
 }
 
 const checkCurrentTemplateVersion = async (macros: MacrosType, isTypeScript: boolean, tmpPath: string) => {
@@ -30,32 +34,29 @@ const checkCurrentTemplateVersion = async (macros: MacrosType, isTypeScript: boo
   }
 }
 
-const checkCurrentRepoVersion = async (repoName: string) => {
-  try {
-    const res = await getResult(`https://registry.npmjs.org/${repoName}`)
-    const latestVersion = res['dist-tags'].latest
-    const localVersion = packageConfig.version
-    if (semver.lt(localVersion, latestVersion)) {
-      const placeholder = '*                                                     *'
-      const origin = '*                                                               *'
-      const latest = 'latest:    ' + chalk.green(latestVersion)
-      const installed = 'installed: ' + chalk.red(localVersion)
-      const longLength = latest.length > installed.length ? latest.length : installed.length
-
-      console.log('*******************************************************')
-      console.log(placeholder)
-      console.log('*', chalk.yellow(` A newer version of ${repoName} is available. `), '*')
-      console.log(placeholder)
-      console.log(generatorPlace(origin, latest, longLength))
-      console.log(generatorPlace(origin, installed, longLength))
-      console.log(placeholder)
-      console.log('*******************************************************')
-    }
-  } catch (err) {
-    // console.log(err)
-  }
-
+const getLocalVersion = (): string => {
+  const packagePath = require.resolve('../../package.json');
+  const packageConfig = require(packagePath)
+  const localVersion = packageConfig.version
+  return localVersion
 }
+
+const getLastVersion = async (repoName: string) => {
+  // TODO：
+  // 1. npm info 替换网络请求
+  // 2. 添加版本缓存，24 小时
+  const res = await getResult(`https://registry.npmjs.org/${repoName}`);
+  const latestVersion = res['dist-tags'].latest;
+  return latestVersion
+}
+
+const generatorSymbol = (count: number, symbol = '*') => {
+  let starSymbol = '';
+  for (let i = 0; i < count; i++) {
+    starSymbol += symbol;
+  }
+  return starSymbol;
+};
 
 const generatorPlace = (origin: string, replace: string, longLength: number): string => {
   const index = Math.round(origin.length / 2) - Math.round(longLength / 2) - 1
@@ -65,8 +66,51 @@ const generatorPlace = (origin: string, replace: string, longLength: number): st
   for (let i = 0; i < length; i++) {
     originArray[i + index] = replaceArray[i]
   }
-
   return originArray.join('')
 }
 
-export { checkRepoVersion, checkCurrentRepoVersion, checkCurrentTemplateVersion }
+const generatorPrompt = ({ latestVersion, localVersion, repoName }: VersionValueType) => {
+  const placeholder = `*${generatorSymbol(53, ' ')}*`
+  const origin = `*${generatorSymbol(63, ' ')}*`
+  const latest = 'latest:    ' + chalk.green(latestVersion)
+  const installed = 'installed: ' + chalk.red(localVersion)
+  const longLength = latest.length > installed.length ? latest.length : installed.length
+
+  console.log(generatorSymbol(55))
+  console.log(placeholder)
+  console.log('*', chalk.yellow(` A newer version of ${repoName} is available. `), '*')
+  console.log(placeholder)
+  console.log(generatorPlace(origin, latest, longLength))
+  console.log(generatorPlace(origin, installed, longLength))
+  console.log(placeholder)
+  console.log(generatorSymbol(55))
+}
+
+const checkCurrentRepoVersion = async (repoName: string) => {
+  try {
+    const latestVersion = await getLastVersion(repoName);
+    const localVersion = getLocalVersion();
+    if (semver.lt(localVersion, latestVersion)) {
+      generatorPrompt({
+        localVersion,
+        latestVersion,
+        repoName
+      });
+      return true
+    } else {
+      return false
+    }
+  } catch (err) {
+    // console.log(err)
+  }
+}
+
+export {
+  getResult,
+  getLocalVersion,
+  getLastVersion,
+  generatorPrompt,
+  checkRepoVersion,
+  checkCurrentRepoVersion,
+  checkCurrentTemplateVersion
+}
