359b3287b9db53ea8d1323ff1a4eb98f18539b7d
大宝 <noyobo@gmail.com>
2020-10-20 10:38:20 +0800

    feat: 增加百度小程序选项

--- src/utils/macros.ts
+++ src/utils/macros.ts
@@ -14,7 +14,8 @@ const macros = {
     { value: 'one', name: '跨平台小程序' },
     { value: 'wechat', name: '微信小程序' },
     { value: 'ali', name: '阿里（支付宝）小程序' },
-    { value: 'toutiao', name: '头条小程序' }
+    { value: 'toutiao', name: '头条小程序' },
+    { value: 'baidu', name: '百度小程序' }
   ]
 }
 
