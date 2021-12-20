cedc48537e7f2835a242d1f21c43233a058d796d
watsonhaw <348748267@qq.com>
2021-12-20 16:25:33 +0800

    fix: 移除创建one应用模板中的title属性，更换为初始化tabbar属性

--- templates/js/template/src/app.config.js
+++ templates/js/template/src/app.config.js
@@ -27,7 +27,40 @@ module.exports.toutiao = {
 
 module.exports.web = {
   pages,
-  title: 'Remax Web Template',
+  window: {
+    // 页面默认标题
+    defaultTitle: 'Remax Web Template',
+    // 是否全局开启下拉刷新
+    pullRefresh: false,
+    // 触底滚动的默认距离，单位 px
+    reachBottomOffset: 50,
+  },
+  router: {
+    // history 类型，支持 hash 和 browser
+    type: 'hash',
+  },
+  // tab bar 配置
+  tabBar: {
+    // 背景色
+    backgroundColor: '#fff',
+    // 选中状态的 tab 标题颜色
+    selectedColor: 'red',
+    // tab 标题颜色
+    titleColor: 'blue',
+    // tab 对象列表
+    items: [
+      {
+        // tab 标题
+        title: '标题',
+        // tab 对应页面路由
+        url: 'pages/index/index',
+        // tab 显示的图片地址
+        image: 'https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ',
+        // tab 选中后的显示的图片地址
+        activeImage: 'https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ',
+      },
+    ],
+  },
 };
 <% } else { %>
 module.exports = {
